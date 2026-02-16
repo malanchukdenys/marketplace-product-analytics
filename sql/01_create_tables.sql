a5fdd6fd6ed03bac28166d5dad15acc66ba60ce3
malanchukdenys <105970786+malanchukdenys@users.noreply.github.com>
2026-02-16 10:33:14 +0200

    Create users, products, and orders tables
    Added SQL statements to create users, products, and orders tables.

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,1 +1,22 @@
+-- USERS TABLE
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    signup_date DATE,
+    country TEXT
+);
 
+-- PRODUCTS TABLE
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    category TEXT,
+    price NUMERIC
+);
+
+-- ORDERS TABLE
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    product_id INT REFERENCES products(product_id),
+    order_date DATE,
+    quantity INT
+);
